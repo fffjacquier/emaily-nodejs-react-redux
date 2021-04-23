c82c55eb76ef7440b42397ec5909f71127553059
Francois Jacquier <fjacquier@gmail.com>
2021-04-23 13:49:21 +0200

    adding requireLogin middleware in express

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,9 +1,11 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.STRIPE_SECRET_KEY);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
-  app.post('/api/stripe', async (req, res) => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     //console.log('BoDY req', req.body, req.user);
+
     const { id } = req.body;
     const charge = await stripe.charges.create({
       amount: 500,
