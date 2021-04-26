e1dca23c075d3648c90dcf7eb71bfe1eab4c6a2f
Francois Jacquier <fjacquier@gmail.com>
2021-04-26 09:10:41 +0200

    add check credits middleware

--- routes/surveysRoutes.js
+++ routes/surveysRoutes.js
@@ -1,1 +1,14 @@
-module.exports = (app) => {};
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+module.exports = (app) => {
+  //app.get('/api/surveys', (req, res) => {});
+
+  // receive feedback from users that clicked on link in the survey
+  //app.post('/api/surveys/webhooks', (req, res) => {});
+
+  // create a survey
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+  });
+};
